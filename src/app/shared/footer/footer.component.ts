01c9beb72330b3123edca0422edf5327ed869536
Helton Bustos <heltonsmith@hotmail.com>
2024-08-31 17:54:16 -0400

    clases y estilos dinamicos

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -10,11 +10,20 @@ export class FooterComponent  implements OnInit {
 
   usuario: string; // Campo para almacenar el nombre del usuario
   private authService = inject(AuthService); // Obtener el servicio de autenticación
+  color: string; // Campo para almacenar el color del footer
 
   constructor() { }
 
   ngOnInit() {
     this.authService.usuario$.subscribe(usuario => this.usuario = usuario); // Obtiene el nombre del usuario logueado
+  }
+
+  getColor() {
+    if (this.usuario === 'admin') {
+      return 'blue';
+    } else {
+      return 'green';
+    }
   }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { DatosPersonalesService } from 'src/app/servicios/datos-personales.service';
 
@@ -7,7 +8,7 @@ import { DatosPersonalesService } from 'src/app/servicios/datos-personales.servi
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent  implements OnInit, OnDestroy {
 
   nombre: string; // Campo para almacenar el nombre
   private sharedService = inject(DatosPersonalesService); // Obtener el servicio de datos personales
@@ -15,11 +16,26 @@ export class HeaderComponent  implements OnInit {
   usuario: string; // Campo para almacenar el nombre del usuario
   private authService = inject(AuthService); // Obtener el servicio de autenticación
 
+  subscriptionDatosPersonales: Subscription; // Subscripción para el observable del nombre del usuario
+  subscriptionAuthService: Subscription; // Subscripción para el observable del estado de autenticación
+
   constructor() { }
 
   ngOnInit() {
-    this.sharedService.nombre$.subscribe(nombre => this.nombre = nombre); // Obtiene el nombre del
-    this.authService.usuario$.subscribe(usuario => this.usuario = usuario); // Obtiene el nombre del usuario logueado
+    this.subscriptionDatosPersonales = this.sharedService.nombre$.subscribe(nombre => {
+      this.nombre = nombre
+      console.log('Header:', nombre);
+    }); // Obtiene el nombre del
+
+    this.subscriptionAuthService = this.authService.usuario$.subscribe(usuario => {
+      this.usuario = usuario
+      console.log('Header:', usuario);
+    }); // Obtiene el nombre del usuario logueado
+  }
+
+  ngOnDestroy() {
+    this.subscriptionDatosPersonales?.unsubscribe(); // Desuscribirse del observable del nombre del usuario
+    this.subscriptionAuthService?.unsubscribe(); // Desuscribirse del observable del estado de autenticación
   }
 
 }
