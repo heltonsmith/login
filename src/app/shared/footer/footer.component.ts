7a54f02c10a9a0bf1fcd2e42fa9af94804d2ab66
Helton Bustos <heltonsmith@hotmail.com>
2024-08-31 03:20:18 -0400

    manejo de menu

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent  implements OnInit {
 
+  usuario: string; // Campo para almacenar el nombre del usuario
+  private authService = inject(AuthService); // Obtener el servicio de autenticación
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authService.usuario$.subscribe(usuario => this.usuario = usuario); // Obtiene el nombre del usuario logueado
+  }
 
 }
